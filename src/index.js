97dcd5b31a052c2b00cae53099b438dba222aa7b
Beka Westberg <bekawestberg@gmail.com>
2022-06-25 19:51:29 -0700

    Webpack and P5

---
+++ src/index.js
@@ -0,0 +1,22 @@
+/**
+ * @license
+ * Copyright 2022 Beka Westberg
+ * SPDX-License-Identifier: MIT
+ */
+
+
+import p5 from 'p5'
+
+const sketch = (p) => {
+  p.setup = function() {
+    p.createCanvas(700, 410);
+  }
+
+  p.draw = function() {
+    p.background(0);
+    p.fill(255);
+    p.rect(100, 100, 50, 50);
+  };
+};
+
+new p5(sketch);
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+    }),
+  ],
+  output: {
+    filename: 'bundle.js',
+  }
+}
