181515c0fcbc8b67717da482affba8452548c066
Beka Westberg <bekawestberg@gmail.com>
2025-05-27 09:36:00 -0700

    chore: clean for new work

--- src/bell.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @license
- * Copyright 2022 Beka Westberg
- * SPDX-License-Identifier: MIT
- */
-
-export function bell(h: number, c: number, w: number, x: number) {
-  return h * Math.exp((-1 * (x - c) * (x - c)) / (2 * w * w));
-}
--- src/index.ts
+++ src/index.ts
@@ -6,16 +6,13 @@
 
 
 import * as p5 from 'p5'
-import {Sunflower} from './sunflower';
 
-const sketch = (p) => {
+const sketch = (p: p5) => {
   const width = 700;
   const height = 410;
-  const flowers = []
 
   p.setup = function() {
     p.createCanvas(width, height);
-    createFlowers(7)
   }
 
   p.draw = function() {
@@ -23,21 +20,7 @@ const sketch = (p) => {
     p.fill(0, 255, 0);
     p.stroke(0, 255, 0);
     p.strokeWeight(4);
-    for (const flower of flowers) {
-      flower.iterate();
-      flower.draw();
-    }
   };
-
-  function createFlowers(c: number) {
-    const delta = width / (c + 1);
-    for (let i = 0; i < c; i++) {
-      flowers.push(new Sunflower(
-        delta * (i + 1) + (Math.random() - 1) * delta / 3,
-        height,
-        p));
-    }
-  }
 };
 
 new p5(sketch);
